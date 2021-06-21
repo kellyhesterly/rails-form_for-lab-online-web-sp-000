5ecefb58665acc7e1336076625490eb79809593c
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-21 22:01:00 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,13 +1,15 @@
 class SchoolClassesController < ApplicationController
 
   def index
+    @school_classes = SchoolClass.all
   end
 
   def show
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def new
-    @school_class = SchoolClass.new 
+    @school_class = SchoolClass.new
   end
 
   def create
