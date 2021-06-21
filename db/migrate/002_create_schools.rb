61085086c8744238fe0e4f48d33d7c0ce1dc8384
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-21 21:43:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_schools.rb
+++ db/migrate/002_create_schools.rb
@@ -1,6 +1,10 @@
 class CreateSchoolsTable < ActiveRecord::Migration
   def change
     create_table :schools do |t|
+      t.string :title
+      t.integer :room_number
+
+      t.timestamps null: false
     end
   end
 end
