033a033c920f55ced793ad81cedabbb18e4fa5c0
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-21 22:00:22 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,9 +7,15 @@ class SchoolClassesController < ApplicationController
   end
 
   def new
+    @school_class = SchoolClass.new 
   end
 
   def create
+    @school_class = SchoolClass.new
+    @school_class[:title] = params[:title]
+    @school_class[:room_number] = params[:room_number]
+    @school.save
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
